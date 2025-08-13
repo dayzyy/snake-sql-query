a14cee7da204f3387d4474d60875097201026db6
Luka Mania <manialuka19@gmail.com>
2025-08-14 02:50:09 +0400

    chore: add factory class for deserializers
    - Implement `DeserializerFactory` class that:
        1. Maps available formats and corresponding deserializer classes
        2. Provides a deserializer class for given format

---
+++ app/deserializers/factory.py
@@ -0,0 +1,19 @@
+from typing import Type
+from deserializers.base_deserializers import BaseDeserializer
+from deserializers.utils.inspect_deserializers import get_deserializer_classes, extract_format_from_deserializer
+
+class DeserializerFactory:
+    DESERIALIZERS: dict[str, Type[BaseDeserializer]] = {
+        extract_format_from_deserializer(cls): cls
+        for cls in get_deserializer_classes()
+    }
+
+    @classmethod
+    def get_deserializer(cls, format: str) -> Type[BaseDeserializer]:
+        try:
+            return cls.DESERIALIZERS[format.lower()]
+        except KeyError:
+            raise ValueError(
+                "Unsupported format!\n"
+                f"Deserialization supported formats: {list(cls.DESERIALIZERS.keys())}"
+            )
--- app/deserializers/utils/inspect_deserializers.py
+++ app/deserializers/utils/inspect_deserializers.py
@@ -3,12 +3,14 @@ from deserializers.base_deserializers import BaseDeserializer
 import inspect
 from typing import List, Type
 
-def get_deserializer_classes():
+def get_deserializer_classes() -> List[Type[BaseDeserializer]]:
     result: List[Type[BaseDeserializer]] = []
 
     for _, obj in inspect.getmembers(deserializers, inspect.isclass):
         if issubclass(obj, BaseDeserializer) and obj is not BaseDeserializer:
             result.append(obj)
+
+    return result
 
 def extract_format_from_deserializer(cls: Type[BaseDeserializer]) -> str:
     name = cls.__name__
