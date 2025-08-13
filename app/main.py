947264bf168abc7544d603ee60cb1625346fe6cb
Luka Mania <manialuka19@gmail.com>
2025-08-14 03:02:01 +0400

    feat: sketch out the main logic
    - add `get_data` function that reads data files and deserializes the data

--- app/main.py
+++ app/main.py
@@ -1,3 +1,25 @@
+from typing import List
+from file_handlers.base_file_readers import TextFileReader 
+from deserializers.factory import DeserializerFactory
+
+def get_data():
+    students_file = '~/path/to/students.json'
+    rooms_file = '~/path/to/rooms.json'
+    format = students_file.split('.')[-1]
+
+    students_data = TextFileReader.read(students_file)
+    rooms_data = TextFileReader.read(rooms_file)
+
+    deserializer = DeserializerFactory.get_deserializer(format)
+
+    students = deserializer.deserialize(students_data)
+    rooms = deserializer.deserialize(rooms_data)
+
+    other_logic(students, rooms)
+
+def other_logic(students: List, rooms: List):
+    pass
+
 def main():
     print("Hello from app!")
 
