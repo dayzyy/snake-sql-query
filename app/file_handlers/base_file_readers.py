f93fa4a88a06c73db1558ed34036d5e8c8f1c68e
Luka Mania <manialuka19@gmail.com>
2025-08-14 00:16:54 +0400

    chore: extend `TextFileReader` class from `BaseFileReader`
    - add `TextFileReader` class for reading any test files (JSON, XML, CSV
      etc)

--- app/file_handlers/base_file_readers.py
+++ app/file_handlers/base_file_readers.py
@@ -3,5 +3,12 @@ from abc import ABC, abstractmethod
 class BaseFileReader(ABC):
     @classmethod
     @abstractmethod
-    def read(cls, file_path: str):
+    def read(cls, file_path: str) -> str:
         raise NotImplementedError
+
+# Supports reading from any text formatted file (JSON, XML, CSV etc.)
+class TextFileReader(BaseFileReader):
+    @classmethod
+    def read(cls, file_path: str) -> str:
+        with open(file_path, 'r', encoding='utf-8') as f:
+            return f.read()
