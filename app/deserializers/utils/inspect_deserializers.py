27a68d9e05f627b1ce3f9fcc54b706c5058dd64c
Luka Mania <manialuka19@gmail.com>
2025-08-14 02:22:20 +0400

    chore: add `extract_format_from_deserializer` method
    - Implement `extract_format_from_deserializer` method to dynamically
      extract format from deserializers.
    - e.g: `extract_format_from_deserializer(JSONDeserialzier) -> JSON`

--- app/deserializers/utils/inspect_deserializers.py
+++ app/deserializers/utils/inspect_deserializers.py
@@ -9,3 +9,13 @@ def get_deserializer_classes():
     for _, obj in inspect.getmembers(deserializers, inspect.isclass):
         if issubclass(obj, BaseDeserializer) and obj is not BaseDeserializer:
             result.append(obj)
+
+def extract_format_from_deserializer(cls: Type[BaseDeserializer]) -> str:
+    name = cls.__name__
+    if name.endswith("Deserializer"):
+        return name[:-len("Deserializer")].lower()
+    else:
+        raise ValueError(
+            "Deserializer class name must end with 'Deserializer'"
+            "e.g: JSONDeserializer, XMLDeserializer, CSVDeserializer"
+        )
