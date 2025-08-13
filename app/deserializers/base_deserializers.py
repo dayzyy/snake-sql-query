2768981e7c1e2493daf1d581b7a4da7dc9e47b4b
Luka Mania <manialuka19@gmail.com>
2025-08-14 00:35:08 +0400

    chore: extend `JSONDeserializer` from `BaseDeserializer`

--- app/deserializers/base_deserializers.py
+++ app/deserializers/base_deserializers.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 from typing import Any
+import json
 
 class BaseDeserializer(ABC):
     @classmethod
@@ -7,3 +8,8 @@ class BaseDeserializer(ABC):
     def deserialize(cls, data: str) -> Any:
         # Convert raw string to standard python object list, dict etc.
         raise NotImplementedError
+
+class JSONDeserializer(BaseDeserializer):
+    @classmethod
+    def deserialize(cls, data: str) -> Any:
+        return json.loads(data)
