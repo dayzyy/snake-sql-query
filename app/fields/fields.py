3e6737f992e8229e08691a1e55f98d75a5de7c1e
Luka Mania <manialuka19@gmail.com>
2025-08-14 20:33:06 +0400

    chore: implement `ForeignKey` for ManyToOne relationship

--- app/fields/fields.py
+++ app/fields/fields.py
@@ -61,3 +61,42 @@ class CharField(Field):
 
     def get_sql_type(self) -> str:
         return "VARCHAR"
+
+class ForeignKey(Field):
+    def __init__(
+            self, table_name: str, column_name: str,
+            pk: bool = False, unique: bool = False, null: bool = True, default=None
+    ) -> None:
+
+        super().__init__(pk=False, unique=False, null=False, default=None)
+        self.table_name = table_name
+        self.column_name = column_name
+        self.referenced_field = self._get_referenced_field()
+
+    def _get_referenced_field(self) -> Field:
+        from models.models import Model
+        all_models = Model.get_models()
+
+        field = None
+        for model in all_models:
+            if model._meta.table_name == self.table_name:
+                for name, value in model._meta.columns.items():
+                    if name == self.column_name:
+                        field = value
+
+        if not field:
+            raise ValueError(f"Invalid reference: table {self.table_name!r} with column {self.column_name!r} does not exist!")
+
+        return field
+
+    def get_python_type(self) -> Any:
+        return self.referenced_field.get_python_type()
+
+    def get_sql_type(self) -> str:
+        return self.referenced_field.get_sql_type()
+
+    def get_sql(self) -> str:
+        default = self.get_default_sql()
+        fk_sql = f"REFERENCES {self.table_name}({self.column_name})"
+
+        return default + ' ' + fk_sql
