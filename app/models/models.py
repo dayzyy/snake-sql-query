2699de52772df899cb53203ad9191041c9bf22eb
Luka Mania <manialuka19@gmail.com>
2025-08-14 19:36:27 +0400

    chore: add `__init__` to `Model`
    - Implement `Model.__init__` which accepts kwargs and automatically sets instance
      attributes based on meta fields

--- app/models/models.py
+++ app/models/models.py
@@ -17,3 +17,17 @@ class Model:
         }
 
         cls._meta = ModelMeta(table_name, fields)
+
+    def __init__(self, **kwargs) -> None:
+        cls = self.__class__
+        for name, field in cls._meta.fields.items():
+            if name in kwargs:
+                value = kwargs[name]
+                field.validate(value)
+                setattr(self, name, value)
+            elif field.default is not None:
+                setattr(self, name, field.default)
+            elif field.null:
+                setattr(self, name, None)
+            else:
+                raise ValueError(f"Missing kwarg for {name!r} attribute in {cls.__name__}.__init__")
