c8a86c5b60e8f863892123197c62eadb176ca625
Luka Mania <manialuka19@gmail.com>
2025-08-15 00:45:48 +0400

    refactor: introduce `Column` class for better `Model` attr storage

--- app/models/models.py
+++ app/models/models.py
@@ -1,10 +1,16 @@
 from fields import fields
 from common.get_classes import get_classes
 from functools import partial
+from dataclasses import dataclass
 import sys
 
+@dataclass
+class Column:
+    name: str
+    field: fields.Field
+
 class ModelMeta:
-    def __init__(self, table_name: str, columns: dict[str, fields.Field]) -> None:
+    def __init__(self, table_name: str, columns: list[Column]) -> None:
         self.table_name = table_name
         self.columns = columns
 
@@ -13,25 +19,21 @@ class Model:
         super().__init_subclass__()
 
         table_name = cls.__name__.lower() + 's'
-
-        columns = {
-            name: value for name, value in cls.__dict__.items()
-            if isinstance(value, fields.Field)
-        }
+        columns = [Column(name, value) for name, value in cls.__dict__.items()]
 
         cls._meta = ModelMeta(table_name, columns)
         cls.get_models = staticmethod(partial(get_classes, Model, sys.modules[cls.__module__]))
 
     def __init__(self, **kwargs) -> None:
         cls = self.__class__
-        for name, field in cls._meta.columns.items():
-            if name in kwargs:
-                value = kwargs[name]
-                field.validate(value)
-                setattr(self, name, value)
-            elif field.default is not None:
-                setattr(self, name, field.default)
-            elif field.null:
-                setattr(self, name, None)
+        for column in cls._meta.columns:
+            if column.name in kwargs:
+                value = kwargs[column.name]
+                column.field.validate(value)
+                setattr(self, column.name, value)
+            elif column.field.default is not None:
+                setattr(self, column.name, column.field.default)
+            elif column.field.null:
+                setattr(self, column.name, None)
             else:
-                raise ValueError(f"Missing kwarg for {name!r} attribute in {cls.__name__}.__init__")
+                raise ValueError(f"Missing kwarg for {column.name!r} attribute in {cls.__name__}.__init__")
