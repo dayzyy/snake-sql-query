0c9554bb0670a433f42aedb3c2d35869de80e102
Luka Mania <manialuka19@gmail.com>
2025-08-15 02:12:49 +0400

    chore: install pytest

---
+++ app/models/custom_models.py
@@ -0,0 +1,13 @@
+from models.models import Model
+from fields import fields
+
+class Room(Model):
+    id = fields.IntField(pk=True)
+    name = fields.CharField()
+
+class Student(Model):
+    id = fields.IntField(pk=True)
+    name = fields.CharField()
+    sex = fields.CharField()
+    birthday = fields.DateTimeField()
+    room = fields.ForeignKey(Room)
