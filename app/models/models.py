3e71c852efaae2d3ecb80e9e79ee3b4eaceea75f
Luka Mania <manialuka19@gmail.com>
2025-08-14 18:26:28 +0400

    refactor: introduce `ModelMeta` for better structured `Model` meta data

--- app/models/models.py
+++ app/models/models.py
@@ -1,16 +1,19 @@
 from fields.fields import Field
 
+class ModelMeta:
+    def __init__(self, table_name: str, fields: dict[str, Field]) -> None:
+        self.table_name = table_name
+        self.fields = fields
+
 class Model:
     def __init_subclass__(cls) -> None:
         super().__init_subclass__()
 
-        meta_data = {}
+        table_name = cls.__name__.lower() + 's'
 
-        meta_data['table_name'] = cls.__name__.lower() + 's'
-
-        meta_data['fields'] = {
+        fields = {
             name: value for name, value in cls.__dict__.items()
             if isinstance(value, Field)
         }
 
-        cls._meta = meta_data
+        cls._meta = ModelMeta(table_name, fields)
